869edaa9a85728984d1e6810060c023563765640
Munes Yasin <86602263+MunesYasin@users.noreply.github.com>
2021-06-30 20:16:08 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+var name =prompt("Whats your name")
+if (name == ""){
+alert("Wlecom ")}
+else if (name =="null"){
+  alert("Welcom Man")}
+  else {
+    alert("Welcom "+name)
+  }
+
+  
+console.log(name)
+
+
+var map=prompt("Whats your favourit map (Sanhok,Erangle,Miramar) ")
+  if(!map){
+      document.write("Why you did not enter your favourite map")}
+
+  else if (map=="Sanhok"){
+    document.write( "Sanhok is the easiest map " +":)" )}
+    else if(map=="Erangle")
+{
+  document.write( "Erangle is the most difficult map "+":)")
+}
+else if(map=="Miramar"){
+  document.write("Miramar is the most beautiful map "+":)")}
+ 
+    
+       else if(map=="") {
+    document.write("Why you did not enter your favourite map")}
+    
+    
+    else {
+      document.write("I dont know about  "+map+" map")
+    }
+    console.log(map)
+      
